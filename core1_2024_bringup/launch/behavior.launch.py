76d86314b5ebb361ce6add6858dfb3a02260e56e
Ar-Ray-code <ray255ar@gmail.com>
2024-03-21 03:16:42 +0900

    update options

--- core1_2024_bringup/launch/behavior.launch.py
+++ core1_2024_bringup/launch/behavior.launch.py
@@ -1,7 +1,7 @@
 from ament_index_python.packages import get_package_share_directory
 import launch
 from launch.actions import DeclareLaunchArgument
-from launch.conditions import UnlessCondition
+from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -11,20 +11,19 @@ def generate_launch_description():
     core1_bt_node = get_package_share_directory('core1_bt_node')
     core1_bt_node_trees = os.path.join(core1_bt_node, 'trees')
 
-    use_rosbag = LaunchConfiguration('use_rosbag', default='false')
-    use_rosbag_arg = DeclareLaunchArgument('use_rosbag', default_value=use_rosbag, description='Use rosbag')
+    use_bt = LaunchConfiguration('use_bt', default='false')
+    use_bt_arg = DeclareLaunchArgument('use_bt', default_value=use_bt, description='Use rosbag')
 
     return launch.LaunchDescription([
-        use_rosbag_arg,
+        use_bt_arg,
         Node(
-            condition=UnlessCondition(use_rosbag),
             package="auto_driver_interface",
             executable="move_to_target_deg_server_exec",
             name="move_to_target_deg_server",
             output="screen",
         ),
         Node(
-            condition=UnlessCondition(use_rosbag),
+            condition=IfCondition(use_bt),
             package="auto_driver_interface",
             executable="tf_to_position_server_exec",
             name="tf_to_position_server",
@@ -35,7 +34,7 @@ def generate_launch_description():
             output="screen",
         ),
         Node(
-            condition=UnlessCondition(use_rosbag),
+            condition=IfCondition(use_bt),
             package="core1_bt_node",
             executable="main",
             name="core1_bt_node",
@@ -45,7 +44,7 @@ def generate_launch_description():
             ],
         ),
         Node(
-            condition=UnlessCondition(use_rosbag),
+            condition=IfCondition(use_bt),
             package="core1_bt_server_py",
             executable="tar_server",
             name="tar_server"
