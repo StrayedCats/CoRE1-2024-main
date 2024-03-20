cb4dffe12a3217c8b11987dff90c0719a728a4e3
Ar-Ray-code <ray255ar@gmail.com>
2024-03-21 02:42:50 +0900

    update behavior launch

--- core1_2024_bringup/launch/behavior.launch.py
+++ core1_2024_bringup/launch/behavior.launch.py
@@ -1,10 +1,16 @@
+from ament_index_python.packages import get_package_share_directory
 import launch
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import UnlessCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+import os
+
 def generate_launch_description():
+    core1_bt_node = get_package_share_directory('core1_bt_node')
+    core1_bt_node_trees = os.path.join(core1_bt_node, 'trees')
+
     use_rosbag = LaunchConfiguration('use_rosbag', default='false')
     use_rosbag_arg = DeclareLaunchArgument('use_rosbag', default_value=use_rosbag, description='Use rosbag')
 
@@ -34,6 +40,9 @@ def generate_launch_description():
             executable="main",
             name="core1_bt_node",
             output="screen",
+            parameters=[
+                {"bt_xml_path": os.path.join(core1_bt_node_trees, "test_panel.xml")}
+            ],
         ),
         Node(
             condition=UnlessCondition(use_rosbag),
