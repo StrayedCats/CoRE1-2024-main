85e469a468325cfdfe95baefdb73b65b1d7c5e1c
Ar-Ray-code <ray255ar@gmail.com>
2024-03-23 19:09:19 +0900

    update exposure

--- core1_2024_bringup/launch/realsense.launch.py
+++ core1_2024_bringup/launch/realsense.launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
                 {"depth_module.profile": "848x480x60"},
                 {"rgb_camera.profile": "848x480x60"},
                 {"rgb_camera.enable_auto_exposure": False},
-                {"rgb_camera.exposure": 10},
+                {"rgb_camera.exposure": 30},
                 {"enable_color": True},
                 {"enable_depth": True},
                 {"enable_infra": False},
